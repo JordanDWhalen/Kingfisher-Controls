9119cb52c76b09c8936f611d038d0547c7bbae99
Jordan Whalen <jordanwhalen@me.com>
2016-04-19 21:58:17 -0400

    General styling, icon fonts, and fun shit

--- dev/js/base.js
+++ dev/js/base.js
@@ -1,3 +1,8 @@
+// Setting up default variables for usage throughout the site
+var large = 1150,
+    medium  = 800,
+    small  = 650;
+
 
 // Checking what the screen size is, in order to accurately adjust the layout of different items
 function minWidth(width) {
@@ -9,31 +14,58 @@ function minWidth(width) {
   }
 }
 
-// Setting the grid up for product line sliders in a way that is friendly to utilizing flexbox for sliding,
-// as well as expanding to show all.
-if( $(".lines").length ){
-  // Getting the item count so I can properly manipulate the width of the line slide wrapper
-  var count = $(".lines .wrapper a").length;
+// Utilizing the minWidth() function to determine how to layout the sliders
+function sliderLayout(target) {
+  if( $(target).length ){
+    // Getting the item count so I can properly manipulate the width of the slide wrapper
+    var count = $(target + " " + ".container a").length;
 
-  if ( minWidth(1150) ) {
-    // If the screen is larger than 1150px, we're going to have 5 columns
+    if ( minWidth(large) ) {
 
-    $(".lines a").css("width", "20%");
-    var number = (count / 5) * 100;
-    $(".lines .wrapper").css("width",  number + "%");
+      // If the screen is larger than 1150px, we're going to have 5 columns
+      $(target + " " + "a").css("width", "20%");
+      var number = (count / 5) * 100;
+      $(target + " " + ".wrapper").css("width",  number + "%");
 
-  } else if ( minWidth(800) ){
-    // If the screen is smaller than 1150px, but larger than 800 we're going to have 3 columns
-    $(".lines a").css("width", "33.33333%");
-    var number = (count / 3) * 100;
-    $(".lines .wrapper").css("width",  number + "%");
+    } else if ( minWidth(medium) ){
 
-  } else {
-    // Otherwise, one column
-    $(".lines a").css("width", "100%");
-    var number = count * 100;
-    $(".lines .wrapper").css("width",  number + "%");
+      // If the screen is smaller than 1150px, but larger than 800 we're going to have 3 columns
+      $(target + " " + "a").css("width", "33.33333%");
+      var number = (count / 3) * 100;
+      $(target + " " + ".wrapper").css("width",  number + "%");
 
+    } else {
+
+      // Otherwise, one column
+      $(target + " " + "a").css("width", "100%");
+      var number = count * 100;
+      $(target + " " + ".wrapper").css("width",  number + "%");
+
+    }
   }
-
 }
+
+// Causing the header navigation background color to fade in as you scroll
+function navigationFade(target, rgb) {
+  var a = $(window).scrollTop() / 100;
+  if(a < .75){
+    $(target).css("background-color", "rgba(" + rgb + "," + a + ")");
+    $("header.global").removeClass("scrolled");
+  } else {
+    $(target).css("background-color", "rgba(" + rgb + "," + ".8" + ")");
+    $("header.global").addClass("scrolled");
+  }
+}
+
+/***************************************
+  Functions to run on load, resize, and scroll
+***************************************/
+sliderLayout(".product-slider");
+
+$(window).resize( function() {
+  sliderLayout(".product-slider");
+});
+
+$(window).scroll( function() {
+  navigationFade("header.global", "255, 255, 255");
+});
